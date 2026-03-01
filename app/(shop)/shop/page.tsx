78e87eb8838bb26a35fc24e55c83fcbecb6974f7
SHRESTH KUMAR <206858669+ShresthChauhan431@users.noreply.github.com>
2026-03-01 18:13:14 +0530

    Fix string quotes in dynamic export

--- app/(shop)/shop/page.tsx
+++ app/(shop)/shop/page.tsx
@@ -1,4 +1,5 @@
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 import Script from 'next/script';
 import { Suspense } from 'react';
